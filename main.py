fc56ffa5f5fa22a8b6af04a0a1b2713ca4ed7aad
Vipul Tripathi <Vipul.Tripathi@neudesic.com>
2021-12-30 16:55:31 +0530

    adding optional user greetings and testing post with pseudo item addition

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    price: float
+    offer: bool
+    description: str
+
 
 app = FastAPI()
 
@@ -6,6 +17,24 @@ app = FastAPI()
 def index():
     return {"message":"Hello world"}
 
+# adding variables in get requests
 @app.get("/greet/{name}")
 def greeting(name:str):
     return {"message":f"Hello {name}"}
+
+# adding optional strings in get requests
+@app.get("/greet")
+def greeting_optional(name:Optional[str]="default user2"):
+    return {"message":f"Hello {name}"}
+
+# adding an item through post/put request
+@app.post("/item/{id}")
+def add_item(id:int, item:Item):
+    return {
+        "id": item.id,
+        "name":item.name,
+        "price":item.price,
+        "offer":item.offer,
+        "description":item.description
+    }
+
